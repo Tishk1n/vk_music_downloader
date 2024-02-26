67e417810a072b4a72f0152beecbc25d4fc3e4c0
Rodion <73991371+Tishk1n@users.noreply.github.com>
2024-02-26 23:17:45 +0500

    FSM

--- src/keyboards.py
+++ src/keyboards.py
@@ -1,6 +1,19 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 menu_button = [
     [KeyboardButton(text='Меню 🚀')]
     ]
 main_kb = ReplyKeyboardMarkup(keyboard=menu_button, resize_keyboard=True)
+
+buttons_list = [
+    [
+    InlineKeyboardButton(text="Поиск плейлистов", callback_data="find_playlist")
+    ],
+    [
+    InlineKeyboardButton(text="Настройки", callback_data="settings")
+    ],
+    [
+    InlineKeyboardButton(text="Регистрация", callback_data="registration")
+    ]
+]
+menu_kb = InlineKeyboardMarkup(inline_keyboard=buttons_list)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
-from config import TOKEN, ADMIN1, ADMIN2
-from aiogram import Bot, Dispatcher, Router, types
+from config import TOKEN
+from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 import logging
 import asyncio
-from aiogram import F
-from keyboards import main_kb
+from aiogram import F, Router
+from keyboards import main_kb, menu_kb
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import CallbackQuery
+from aiogram.fsm.context import FSMContext
 
 dp = Dispatcher()
 bot = Bot(TOKEN)
+
+router = Router()
+
+class Registration(StatesGroup):
+    name = State()
+    age = State()
+    number = State()
 
 
 @dp.message(CommandStart())
@@ -16,8 +26,34 @@ async def start_def(message: types.Message):
 
 @dp.message(F.text == "Меню 🚀")
 async def main_menu(message: types.Message):
-    await bot.send_message(text="Приветствую в меню", chat_id=message.from_user.id)
-    await start_def(message)
+    await bot.send_message(text="Приветствую в меню", chat_id=message.from_user.id, reply_markup=menu_kb)
+
+@dp.callback_query(F.data == "registration")
+async def start_registration(call: CallbackQuery, state: FSMContext):
+    await state.set_state(Registration.name)
+    await call.message.answer("Введите Ваше имя.")
+
+@dp.message(Registration.name)
+async def fsm_age(message: types.Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer("Введите Ваш возраст")
+    await state.set_state(Registration.age)
+
+@dp.message(Registration.age)
+async def fsm_number(message: types.Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer("Введите Ваш номер телефона.")
+    await state.set_state(Registration.number)
+
+@dp.message(Registration.number)
+async def fsm_end(message: types.Message, state:FSMContext):
+    await state.update_data(number=message.text)
+    await message.answer("Спасибо за регистрацию! Загрузка ваших данных.")
+    data = await state.get_data()
+    await message.answer(f"Ваше имя: {data['name']}\nВаш возраст {data['age']}\nВаш номер телефона: {data['number']}")
+    print(f"Ваше имя: {data['name']}\nВаш возраст {data['age']}\nВаш номер телефона: {data['number']}")
+    await state.clear()
+
 
 async def main() -> None:
     await dp.start_polling(bot)
